96ce825a83d9fe9a2bf7a0a169c0ef2836eaac46
Gelli4k <gelli4ka@mail.ru>
2022-12-11 09:55:08 +0300

    lesson-35 final VK

--- todolist/urls.py
+++ todolist/urls.py
@@ -18,10 +18,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.urls'), name='core'),
+    path('core/', include('core.urls'), name='core'),
     # path('goals/', include('goals.urls'), name='goals'),
 
-    path('oauth/', include('social_django.urls', namespace='social')),
+    path('oauth', include('social_django.urls', namespace='social')),
 
     # path('docs/schema/', SpectacularAPIView.as_view(), name='schema'),
     # path('docs/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
