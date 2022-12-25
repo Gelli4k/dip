39b9b5475be1aba7985b700003d15d0c36eb6b1a
Gelli4k <gelli4ka@mail.ru>
2022-12-26 02:20:27 +0300

    lesson-38 step7 in process

--- bot/views.py
+++ bot/views.py
@@ -19,7 +19,7 @@ class BotVerifyView(generics.UpdateAPIView):
         tg_client = TgClient('5806733281:AAGBZWpPnL-qRs8OTY7dyx0Xz3J_cwsJJYg')
         tg_user = TgUser.objects.filter(verification_code=data['verification_code']).first()
         if not tg_user:
-            Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
         tg_user.user = request.user
         tg_user.save()
         tg_client.send_message(chat_id=tg_user.tg_chat_id, text='Успешно!')
