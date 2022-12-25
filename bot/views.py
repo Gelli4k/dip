7b9cf3674b1130e3cd6331be6a39700604c0e84f
Gelli4k <gelli4ka@mail.ru>
2022-12-26 02:05:47 +0300

    lesson-38 step7 in process

--- bot/views.py
+++ bot/views.py
@@ -17,7 +17,7 @@ class BotVerifyView(generics.UpdateAPIView):
     def patch(self, request, *args, **kwargs):
         data = self.serializer_class(request.data).data
         tg_client = TgClient('5806733281:AAGBZWpPnL-qRs8OTY7dyx0Xz3J_cwsJJYg')
-        tg_user = TgUser.objects.filter(verification_code=data['verification_code']).exists()
+        tg_user = TgUser.objects.filter(verification_code=data['verification_code']).first()
         if not tg_user:
             raise Exception
         tg_user.user = request.user
