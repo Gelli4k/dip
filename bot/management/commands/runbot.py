0092b3e0196c20decc014709f2f712c4b9465dcc
Gelli4k <gelli4ka@mail.ru>
2022-12-27 08:14:44 +0300

    lesson-38 end runbot.py

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -11,9 +11,9 @@ from todolist.settings import TG_BOT_API_TOKEN
 class Command(BaseCommand):
     help = 'Runs telegram bot'
     tg_client = TgClient(TG_BOT_API_TOKEN)
+    offset = 0
 
     def choose_category(self, msg: Message, tg_user: TgUser):
-        global offset
         goal_categories = GoalCategory.objects.filter(
             board__participants__user=tg_user.user,
             is_deleted=False,
@@ -26,9 +26,9 @@ class Command(BaseCommand):
         is_running = True
 
         while is_running:
-            res = self.tg_client.get_updates(offset=offset)
+            res = self.tg_client.get_updates(offset=self.offset)
             for item in res.result:
-                offset = item.update_id + 1
+                self.offset = item.update_id + 1
                 if hasattr(item, 'message'):
                     category = goal_categories.filter(title=msg.text)
                     if category:
@@ -47,18 +47,16 @@ class Command(BaseCommand):
                         )
                         is_running = False
 
-
     def create_goal(self, msg: Message, tg_user: TgUser, category: GoalCategory):
-        global offset
         self.tg_client.send_message(
-                chat_id=msg.chat.id,
-                text=f'Введите пожалуйста заголовок для создания цели'
+            chat_id=msg.chat.id,
+            text=f'Введите пожалуйста заголовок для создания цели'
         )
         is_running = True
         while is_running:
-            res = self.tg_client.get_updates(offset=offset)
+            res = self.tg_client.get_updates(offset=self.offset)
             for item in res.result:
-                offset = item.update_id + 1
+                self.offset = item.update_id + 1
                 if item.message.text == '/cancel':
                     self.tg_client.send_message(
                         chat_id=msg.chat.id,
@@ -77,7 +75,6 @@ class Command(BaseCommand):
                     )
                     is_running = False
 
-
     def get_goals(self, msg: Message, tg_user: TgUser):
         goals = Goal.objects.filter(category__board__participants__user=tg_user.user,
                                     ).exclude(status=Goal.Status.archived)
@@ -86,7 +83,6 @@ class Command(BaseCommand):
             chat_id=msg.chat.id,
             text=f'Вот список ваших целей:\n {goals_str}'
         )
-
 
     def handle_message(self, msg: Message):
         tg_user, created = TgUser.objects.get_or_create(
@@ -99,9 +95,9 @@ class Command(BaseCommand):
             self.tg_client.send_message(
                 chat_id=msg.chat.id,
                 text=f"Подтвердите пожалуйста свой аккаунт."
-                    f"Для подтверждения необходимо ввести код:{tg_user.verification_code} на сайте"
+                     f"Для подтверждения необходимо ввести код:{tg_user.verification_code} на сайте"
             )
-        elif msg.text == '/goals':
+        if msg.text == '/goals':
             self.get_goals(msg, tg_user)
         elif msg.text == '/create':
             self.choose_category(msg, tg_user)
@@ -111,12 +107,10 @@ class Command(BaseCommand):
                 text=f'Не знаю такой команды {msg.text}'
             )
 
-
     def handle(self, *args, **options):
-        offset = 0
         while True:
-            res = self.tg_client.get_updates(offset=offset)
+            res = self.tg_client.get_updates(offset=self.offset)
             for item in res.result:
-                offset = item.update_id + 1
+                self.offset = item.update_id + 1
                 if hasattr(item, 'message'):
                     self.handle_message(item.message)
